0664dd0fb87c00e48b3161e646cfb2413301c215
BaoTheTran <ttbao2710@gmail.com>
2023-07-12 16:10:58 +0700

    loading

--- src/booking-ticket/seat.js
+++ src/booking-ticket/seat.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 import Item from './item';
+import { connect } from 'react-redux';
+class Seat extends Component {
+  renderListItem=()=>{
+    let {listItem,danhSachGhe} = this.props;
+    return listItem.map((item)=>{
+        return (
+          <div key={item.hang}>
+              {item.danhSachGhe.map((ghe)=>{
+                return(
+                  <Item
+                  key={ghe.soGhe}
+                  ghe={ghe}                  
+                  />
+                )
+              })}
+          </div>
+        )
+    })
+  }
 
-export default class Seat extends Component {
   render() {
     return (
-      <div>seat</div>
+      <div className="seatStructure txt-center" style={{overflowX: 'auto'}}>
+      <p id="notification" />
+      <table id="seatsBlock">
+        <Item/>
+      </table>
+      <div className="screen">
+        <h2 className="wthree">Screen this way</h2>
+      </div>
+      <button onclick="updateTextArea()">Confirm Selection</button>
+    </div>
     )
   }
 }
+
+const mapStateToProps = (state)=>{
+  return{
+    //key : listUser la props cho Component Users
+    listItem : state.itemReducer.listItem,
+    danhSachGhe : state.itemReducer.danhSachGhe,
+  }
+}
+
+export default connect(mapStateToProps,null) (Seat);
---
+++ src/store/itemReducer.js
@@ -0,0 +1,62 @@
+import data from "../data.json";
+const initialState ={
+    listItem : data,
+    danhSachGhe : data.danhSachGhe,
+}
+
+const itemReducer =(state = initialState, action)=>{
+    console.log( state,action);
+    switch (action.type) {
+        case "DELETE USER":{
+          //xoa user
+          let listUserClone =[...state.listUser];
+          //tim vi tri
+          const index = listUserClone.findIndex((user)=>user.id===action.payload);
+          if(index !== -1){
+            listUserClone.splice(index,1);
+  
+            //cap nhat lai state
+            state.listUser = listUserClone;
+          }
+          return {...state};
+        }
+  
+        case "GET_KEYWORD":{
+          state.keywords = action.payload;
+  
+          return {...state};
+        }
+  
+        case "SUBMIT_USER":{
+          const user = action.payload
+          let listUserClone = [...state.listUser];
+          if(user.id){
+            //update
+            const index = listUserClone.findIndex((newUser)=>newUser.id ===user.id)
+            if(index !== -1){
+              listUserClone[index] = user;
+            }
+          }else{
+            //add
+            const userClone = {...user,id :new Date().getTime()}
+            listUserClone.push(userClone)
+          }
+  
+          //cap nhat lai state
+          state.listUser = listUserClone; 
+          
+          return {...state};
+        }
+  
+        case "EDIT_USER":{
+          state.userEdit = action.payload;
+  
+          return {...state}
+        }
+      
+        default:
+          return {...state};
+      }
+}
+
+export default itemReducer;
